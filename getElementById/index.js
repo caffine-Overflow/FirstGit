f720699bec8149dc0c0f723be8370fe8c6a32681
caffine_Overflow <tnzajk@gmail.com>
2024-07-28 17:59:01 +0530

    js for getElementById

---
+++ getElementById/index.js
@@ -0,0 +1,14 @@
+
+let mainHeading=document.getElementById("main-heading");
+mainHeading.textContent="Fruit World";
+mainHeading.style.color="orange";
+
+let header=document.getElementById("header");
+header.style.backgroundColor="green";
+header.style.borderBottom="orange";
+
+let fruitInBasket=document.getElementById("basket-heading");
+fruitInBasket.style.color="green";
+
+let thanks=document.getElementById("thanks");
+thanks.innerHTML="<p>Please visit us again</p>";
